1a6ecc32043c36a1ed65bb202740279d2a666b3f
faridtuf <ghulam.farid@tuf.edu.pk>
2022-11-26 17:09:14 +0500

    Added Admin Controller

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -94,6 +94,240 @@
                     </ul>
                 </li>
             @endcanany
+            @canany(['category-list','category-create'])
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle waves-effect" href="#" id="category" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span>
+                        Category
+                    </span>
+                        <i class="fa fa-plus"></i>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="category">
+                        @can('category-create')
+                            <li><a class="dropdown-item" href="{{route('category.create')}}">Add New</a></li>
+                        @endcan
+                        @can('category-list')
+                            <li><a class="dropdown-item" href="{{route('category.index')}}">View All</a></li>
+                        @endcan
+                    </ul>
+                </li>
+            @endcanany
+            @canany(['season-list','season-create'])
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle waves-effect" href="#" id="season" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span>
+                        Season
+                    </span>
+                        <i class="fa fa-plus"></i>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="category">
+                        @can('season-create')
+                            <li><a class="dropdown-item" href="{{route('season.create')}}">Add New</a></li>
+                        @endcan
+                        @can('season-list')
+                            <li><a class="dropdown-item" href="{{route('season.index')}}">View All</a></li>
+                        @endcan
+                    </ul>
+                </li>
+            @endcanany
+            @canany(['age-group-list','age-group-create'])
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle waves-effect" href="#" id="age-group" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span>
+                        Age Group
+                    </span>
+                        <i class="fa fa-plus"></i>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="age-group">
+                        @can('age-group-create')
+                            <li><a class="dropdown-item" href="{{route('age-group.create')}}">Add New</a></li>
+                        @endcan
+                        @can('age-group-list')
+                            <li><a class="dropdown-item" href="{{route('age-group.index')}}">View All</a></li>
+                        @endcan
+                    </ul>
+                </li>
+            @endcanany
+            @canany(['country-list','country-create'])
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle waves-effect" href="#" id="country" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span>
+                        Country
+                    </span>
+                        <i class="fa fa-plus"></i>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="country">
+                        @can('country-create')
+                            <li><a class="dropdown-item" href="{{route('country.create')}}">Add New</a></li>
+                        @endcan
+                        @can('country-list')
+                            <li><a class="dropdown-item" href="{{route('country.index')}}">View All</a></li>
+                        @endcan
+                    </ul>
+                </li>
+            @endcanany
+            @canany(['region-list','region-create'])
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle waves-effect" href="#" id="region" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span>
+                        Region
+                    </span>
+                        <i class="fa fa-plus"></i>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="region">
+                        @can('region-create')
+                            <li><a class="dropdown-item" href="{{route('region.create')}}">Add New</a></li>
+                        @endcan
+                        @can('region-list')
+                            <li><a class="dropdown-item" href="{{route('region.index')}}">View All</a></li>
+                        @endcan
+                    </ul>
+                </li>
+            @endcanany
+            @canany(['customer-list','customer-create'])
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle waves-effect" href="#" id="customer" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span>
+                        Customer
+                    </span>
+                        <i class="fa fa-plus"></i>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="customer">
+                        @can('customer-create')
+                            <li><a class="dropdown-item" href="{{route('customer.create')}}">Add New</a></li>
+                        @endcan
+                        @can('customer-list')
+                            <li><a class="dropdown-item" href="{{route('customer.index')}}">View All</a></li>
+                        @endcan
+                    </ul>
+                </li>
+            @endcanany
+            @canany(['loom-type-list','loom-type-create'])
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle waves-effect" href="#" id="loom-type" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span>
+                        Loom Type
+                    </span>
+                        <i class="fa fa-plus"></i>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="loom-type">
+                        @can('loom-type-create')
+                            <li><a class="dropdown-item" href="{{route('loom-type.create')}}">Add New</a></li>
+                        @endcan
+                        @can('loom-type-list')
+                            <li><a class="dropdown-item" href="{{route('loom-type.index')}}">View All</a></li>
+                        @endcan
+                    </ul>
+                </li>
+            @endcanany
+            @canany(['atribute-yarn-list','atribute-yarn-create'])
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle waves-effect" href="#" id="atribute-yarn" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span>
+                        Attribute Yarn
+                    </span>
+                        <i class="fa fa-plus"></i>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="atribute-yarn">
+                        @can('atribute-yarn-create')
+                            <li><a class="dropdown-item" href="{{route('atribute-yarn.create')}}">Add New</a></li>
+                        @endcan
+                        @can('atribute-yarn-list')
+                            <li><a class="dropdown-item" href="{{route('atribute-yarn.index')}}">View All</a></li>
+                        @endcan
+                    </ul>
+                </li>
+            @endcanany
+            @canany(['atribute-weaving-list','atribute-weaving-create'])
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle waves-effect" href="#" id="atribute-weaving" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span>
+                        Attribute Weaving
+                    </span>
+                        <i class="fa fa-plus"></i>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="atribute-weaving">
+                        @can('atribute-weaving-create')
+                            <li><a class="dropdown-item" href="{{route('atribute-weaving.create')}}">Add New</a></li>
+                        @endcan
+                        @can('atribute-weaving-list')
+                            <li><a class="dropdown-item" href="{{route('atribute-weaving.index')}}">View All</a></li>
+                        @endcan
+                    </ul>
+                </li>
+            @endcanany
+            @canany(['atribute-processing-list','atribute-processing-create'])
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle waves-effect" href="#" id="atribute-processing" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span>
+                        Attribute Processing
+                    </span>
+                        <i class="fa fa-plus"></i>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="atribute-processing">
+                        @can('atribute-processing-create')
+                            <li><a class="dropdown-item" href="{{route('atribute-processing.create')}}">Add New</a></li>
+                        @endcan
+                        @can('atribute-processing-list')
+                            <li><a class="dropdown-item" href="{{route('atribute-processing.index')}}">View All</a></li>
+                        @endcan
+                    </ul>
+                </li>
+            @endcanany
+            @canany(['atribute-stitching-list','atribute-stitching-create'])
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle waves-effect" href="#" id="atribute-stitching" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span>
+                        Attribute Stitching
+                    </span>
+                        <i class="fa fa-plus"></i>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="atribute-processing">
+                        @can('atribute-stitching-create')
+                            <li><a class="dropdown-item" href="{{route('atribute-stitching.create')}}">Add New</a></li>
+                        @endcan
+                        @can('atribute-stitching-list')
+                            <li><a class="dropdown-item" href="{{route('atribute-stitching.index')}}">View All</a></li>
+                        @endcan
+                    </ul>
+                </li>
+            @endcanany
+            @canany(['fabric-type-list','fabric-type-create'])
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle waves-effect" href="#" id="fabric-type" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span>
+                        Fabric Type
+                    </span>
+                        <i class="fa fa-plus"></i>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="fabric-type">
+                        @can('fabric-type-create')
+                            <li><a class="dropdown-item" href="{{route('fabric-type.create')}}">Add New</a></li>
+                        @endcan
+                        @can('fabric-type-list')
+                            <li><a class="dropdown-item" href="{{route('fabric-type.index')}}">View All</a></li>
+                        @endcan
+                    </ul>
+                </li>
+            @endcanany
+            @canany(['product-list','product-create'])
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle waves-effect" href="#" id="product" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span>
+                        Product
+                    </span>
+                        <i class="fa fa-plus"></i>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="product">
+                        @can('product-create')
+                            <li><a class="dropdown-item" href="{{route('product.create')}}">Add New</a></li>
+                        @endcan
+                        @can('product-list')
+                            <li><a class="dropdown-item" href="{{route('product.index')}}">View All</a></li>
+                        @endcan
+                    </ul>
+                </li>
+            @endcanany
         </ul>
     </nav>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,22 @@
 <?php
 
+use App\Http\Controllers\Admin\AgeGroupController;
+use App\Http\Controllers\Admin\AtributeProcessingController;
+use App\Http\Controllers\Admin\AtributeStitchingController;
+use App\Http\Controllers\Admin\AtributeWeavingController;
+use App\Http\Controllers\Admin\AtributeYarnController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\CountryController;
+use App\Http\Controllers\Admin\CustomerController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\FabricTypeController;
+use App\Http\Controllers\Admin\LoomTypeController;
 use App\Http\Controllers\Admin\PermissionController;
 use App\Http\Controllers\Admin\PermissionGroupController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\RegionController;
 use App\Http\Controllers\Admin\RoleController;
+use App\Http\Controllers\Admin\SeasonController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\front\HomeController;
 use Illuminate\Support\Facades\Route;
@@ -22,12 +35,54 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class,'index'])->name('main');
 Route::group(['middleware' => ['auth','verified','IsActive','xss'],'prefix'=>'admin'], function() {
     Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
+    //Permissions
     Route::resource('permission_group',PermissionGroupController::class);
     Route::resource('permission',PermissionController::class);
+    //Roles
     Route::resource('role',RoleController::class);
     Route::get('get/roles',[RoleController::class,'getRoles'])->name('getRoles');
+    //User
     Route::resource('user',UserController::class);
     Route::get('get/users',[UserController::class,'getUsers'])->name('getUsers');
+    //Category
+    Route::resource('category',CategoryController::class);
+    Route::get('get/categories',[CategoryController::class,'getCategories'])->name('getCategories');
+    //Season
+    Route::resource('season',SeasonController::class);
+    Route::get('get/seasons',[SeasonController::class,'getSeasons'])->name('getSeasons');
+    //Age Group
+    Route::resource('age-group',AgeGroupController::class);
+    Route::get('get/age-groups',[AgeGroupController::class,'getAgeGroups'])->name('getAgeGroups');
+    //Country
+    Route::resource('country',CountryController::class);
+    Route::get('get/countries',[CountryController::class,'getCountries'])->name('getCountries');
+    //Regions
+    Route::resource('region',RegionController::class);
+    Route::get('get/regions',[RegionController::class,'getRegions'])->name('getRegions');
+    //Customers
+    Route::resource('customer',CustomerController::class);
+    Route::get('get/customers',[CustomerController::class,'getCustomers'])->name('getCustomers');
+    //Loom Type
+    Route::resource('loom-type',LoomTypeController::class);
+    Route::get('get/loom-types',[LoomTypeController::class,'getLoomTypes'])->name('getLoomTypes');
+    //Attribute Yarn
+    Route::resource('atribute-yarn',AtributeYarnController::class);
+    Route::get('get/atribute-yarns',[AtributeYarnController::class,'getAtributeYarn'])->name('getAtributeYarn');
+    //Attribute Weaving
+    Route::resource('atribute-weaving',AtributeWeavingController::class);
+    Route::get('get/atribute-weavings',[AtributeWeavingController::class,'getAtributeWeaving'])->name('getAtributeWeaving');
+    //Attribute Processing
+    Route::resource('atribute-processing',AtributeProcessingController::class);
+    Route::get('get/atribute-processings',[AtributeProcessingController::class,'getAtributeProcessing'])->name('getAtributeProcessing');
+    //Attribute Stitching
+    Route::resource('atribute-stitching',AtributeStitchingController::class);
+    Route::get('get/atribute-stitchings',[AtributeStitchingController::class,'getAtributeStitching'])->name('getAtributeStitching');
+    //Fabric Type
+    Route::resource('fabric-type',FabricTypeController::class);
+    Route::get('get/fabric-types',[FabricTypeController::class,'getFabricType'])->name('getFabricType');
+    //Product
+    Route::resource('product',ProductController::class);
+    Route::get('get/products',[ProductController::class,'getProducts'])->name('getProducts');
 });
 Route::get('/logout', function () {
     Auth::logout();
